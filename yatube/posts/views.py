926c9abd6034736c6f9a744378e46b8a1c52a9f8
svanonline <101625280+svanonline@users.noreply.github.com>
2022-06-13 13:17:16 +0300

    Update views.py

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -144,11 +144,11 @@ def profile_unfollow(request, username):
 def page_not_found(request, exception):
     return render(
         request,
-        'misc/404.html',
+        'core/404.html',
         {'path': request.path},
         status=404
     )
 
 
 def server_error(request):
-    return render(request, "misc/500.html", status=500)
+    return render(request, "core/500.html", status=500)
