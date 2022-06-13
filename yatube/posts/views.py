abe97986e26d38727fbfbd5f5ea82cf6c4c2ffd7
svanonline <101625280+svanonline@users.noreply.github.com>
2022-06-13 13:45:55 +0300

    Update views.py

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -151,4 +151,4 @@ def page_not_found(request, exception):
 
 
 def server_error(request):
-    return render(request, "core/500.html", status=500)
+    return render(request, "core/403csrf.html", status=403)
