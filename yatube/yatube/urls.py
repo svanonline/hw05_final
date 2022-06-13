b6afe555cf8fbf243f987f89d74be0219efb3c24
svanonline <101625280+svanonline@users.noreply.github.com>
2022-06-13 14:03:33 +0300

    Update urls.py

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -3,10 +3,11 @@ from django.urls import include, path
 
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import handler404, handler403
+from django.conf.urls import handler404, handler403, handler500
 
 handler404 = 'core.views.page_not_found'
 handler403 = "posts.views.server_error"
+handler500 = "posts.views.server_error"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
